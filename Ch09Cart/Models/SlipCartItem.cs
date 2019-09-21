46ecb4746a821972b20fc38ea44d3a8e3fa2ea8c
欣 刘 <liuxinliu_01@hotmail.com>
2019-09-21 10:19:01 -0600

    Added some other classes including cartitems

---
+++ Ch09Cart/Models/SlipCartItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ch09Cart.Models
+{
+    public class SlipCartItem
+    {
+        public SlipCartItem() { }
+
+        public SlipCartItem(Slip slip, int quantity)
+        {
+            this.Slip = slip;
+            this.Quantity = quantity;
+        }
+
+        public Slip Slip { get; set; }
+        public int Quantity { get; set; }
+
+        public void AddQuantity(int quantity)
+        {
+            this.Quantity += quantity;
+        }
+
+        public string Display()
+        {
+            string displayString = string.Format("{0} ({1})",
+               Slip.DockID,
+                Quantity.ToString()
+               
+            );
+            return displayString;
+        }
+    }
+}
---
+++ Ch09Cart/Models/SlipCartItemList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ch09Cart.Models
+{
+    public class SlipCartItemList
+    {
+        private List<SlipCartItem> slipcartItems;
+
+        public SlipCartItemList()
+        {
+            slipcartItems = new List<SlipCartItem>();
+        }
+
+        public int Count
+        {
+            get { return slipcartItems.Count; }
+        }
+
+        public SlipCartItem this[int index]
+        {
+            get { return slipcartItems[index]; }
+            set { slipcartItems[index] = value; }
+        }
+
+        public SlipCartItem this[int id]
+        {
+            get
+            {
+                foreach (SlipCartItem c in slipcartItems)
+                    if (c.Slip.SlipID == id) return c;
+                return null;
+            }
+        }
+
+        public static SlipCartItemList GetCart()
+        {
+            SlipCartItemList cart = (SlipCartItemList)HttpContext.Current.Session["Cart"];
+            if (cart == null)
+                HttpContext.Current.Session["Cart"] = new CartItemList();
+            return (SlipCartItemList)HttpContext.Current.Session["Cart"];
+        }
+
+        public void AddItem(Slip Slip, int quantity)
+        {
+            SlipCartItem c = new SlipCartItem(Slip, quantity);
+            slipcartItems.Add(c);
+        }
+
+        public void RemoveAt(int index)
+        {
+            slipcartItems.RemoveAt(index);
+        }
+
+        public void Clear()
+        {
+            slipcartItems.Clear();
+        }
+    }
+}
