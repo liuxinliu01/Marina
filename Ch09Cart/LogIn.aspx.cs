6426099a158b9fdb67814ad4a09a7dbd05661d5b
Ikenna Okoro <okorokingsley444@yahoo.com>
2019-09-21 16:02:41 -0600

    Kingsley added log in and register

---
+++ Ch09Cart/LogIn.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ch09Cart
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginButton_Click(object sender, EventArgs e)
+        {
+            String CS = ConfigurationManager.ConnectionStrings["MarinaConnectionString"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(CS))
+            {
+                string query = " SELECT * from Customer where Firstname ='" + fname.Text + "' and Lastname='" + lname.Text + "'";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataAdapter dataReader = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                dataReader.Fill(dt);
+
+                if (dt.Rows.Count != 0)
+                {
+                    Session["ID"] = dt.Rows[0]["ID"].ToString();
+                    Session["Firstname"] = fname.Text;
+                    Response.Redirect("~/Register.aspx");
+                    //lblError.Text = Session["ID"].ToString();
+                }
+                else
+                {
+                    lblError.Text = "Invalid Username or Password";
+                }
+
+            }
+        }
+    }
+}
---
+++ Ch09Cart/Register.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ch09Cart
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void registerButton_Click(object sender, EventArgs e)
+        {
+            if (fnameTextBox.Text != "" && lnameTextBox.Text != "" && phoneTextBox.Text != "" && cityTextBox.Text != "")
+            {
+                String CS = ConfigurationManager.ConnectionStrings["MarinaConnectionString"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(CS))
+                {
+                    string query = "INSERT INTO Customer(FirstName,LastName,Phone,City) VALUES(@FirstName,@LastName,@Phone,@city)";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    connection.Open();
+                    cmd.Parameters.AddWithValue("@FirstName", fnameTextBox.Text);
+                    cmd.Parameters.AddWithValue("@LastName", lnameTextBox.Text);
+                    cmd.Parameters.AddWithValue("@Phone", phoneTextBox.Text);
+                    cmd.Parameters.AddWithValue("@City", cityTextBox.Text);
+                    cmd.ExecuteNonQuery();
+                    lblError.Text = "Registration Successfull";
+                    lblError.ForeColor = Color.Green;
+
+                }
+            }
+            else
+            {
+                lblError.ForeColor = Color.Red;
+                lblError.Text = "All field are Mandatory";
+            }
+        }
+    }
+}
